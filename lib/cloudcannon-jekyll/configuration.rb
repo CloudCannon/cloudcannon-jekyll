8dadf1c597dd406497e148183616c56a12f1e1c8
Ross Phillips <rphillips-nz@users.noreply.github.com>
2019-11-20 11:20:57 +1300

    Version

--- lib/cloudcannon-jekyll/configuration.rb
+++ lib/cloudcannon-jekyll/configuration.rb
@@ -2,39 +2,37 @@
 
 module CloudCannonJekyll
   class Configuration
-    class << self
-      def processed?(site)
-        site.instance_variable_get(:@_cloudcannon_jekyll_processed) == true
+    def self.processed?(site)
+      site.instance_variable_get(:@_cloudcannon_jekyll_processed) == true
+    end
+
+    def self.process(site)
+      site.instance_variable_set :@_cloudcannon_jekyll_processed, true
+    end
+
+    def self.overridden_config(user_config)
+      config = Jekyll::Utils.deep_merge_hashes(Jekyll::Configuration::DEFAULTS, user_config)
+      config = config.add_default_collections if config.respond_to? :add_default_collections
+      config = config.fix_common_issues if config.respond_to? :fix_common_issues
+      config = config.add_default_excludes if config.respond_to? :add_default_excludes
+
+      key = Jekyll::VERSION.start_with?("2") ? "gems" : "plugins"
+
+      config[key] = Array(config[key])
+      config[key].push("cloudcannon-jekyll") unless config[key].include? "cloudcannon-jekyll"
+      config
+    end
+
+    def self.set(site)
+      return if processed? site
+
+      if site.respond_to? :config=
+        site.config = overridden_config(site.config)
+      else # Jekyll pre 3.5
+        site.instance_variable_set :@config, overridden_config(site.config)
       end
 
-      def process(site)
-        site.instance_variable_set :@_cloudcannon_jekyll_processed, true
-      end
-
-      def overridden_config(user_config)
-        config = Jekyll::Utils.deep_merge_hashes(Jekyll::Configuration::DEFAULTS, user_config)
-        config = config.add_default_collections if config.respond_to? :add_default_collections
-        config = config.fix_common_issues if config.respond_to? :fix_common_issues
-        config = config.add_default_excludes if config.respond_to? :add_default_excludes
-
-        key = Jekyll::VERSION.start_with?("2") ? "gems" : "plugins"
-
-        config[key] = Array(config[key])
-        config[key].push("cloudcannon-jekyll") unless config[key].include? "cloudcannon-jekyll"
-        config
-      end
-
-      def set(site)
-        return if processed? site
-
-        if site.respond_to? :config=
-          site.config = overridden_config(site.config)
-        else # Jekyll pre 3.5
-          site.instance_variable_set :@config, overridden_config(site.config)
-        end
-
-        process(site)
-      end
+      process(site)
     end
   end
 end
